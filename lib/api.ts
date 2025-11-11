fb05f0ce9b349d20ade4055bd472a59fbb413480
Maryam <mm.saleh.88@gmail.com>
2025-11-11 23:07:27 +0330

    Do some clean up on api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,10 +1,5 @@
-import {
-  Application,
-  ApplicationCreate,
-  ApplicationRow,
-  ApplicationUpdate,
-} from "@/app/types";
-import { toApiPayload, fromApiResponse } from "./mappers";
+import { Application, ApplicationRow } from "@/app/types";
+import { fromApiResponse } from "./mappers";
 import { supabaseAdmin } from "./supabaseAdmin";
 
 const BASE_URL = "/api/applications";
@@ -36,42 +31,3 @@ export async function getApplication(id: string): Promise<Application> {
   const row: ApplicationRow = await res.json();
   return fromApiResponse(row);
 }
-
-// export async function createApplication(
-//   data: ApplicationCreate
-// ): Promise<Application> {
-//   const res = await fetch(BASE_URL, {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify(toApiPayload(data)),
-//   });
-
-//   if (!res.ok) {
-//     throw new Error("Failed to create application");
-//   }
-//   const result: ApplicationRow = await res.json();
-
-//   return fromApiResponse(result);
-// }
-
-// export async function updateApplication(id: string, data: ApplicationUpdate) {
-//   const res = await fetch(`${BASE_URL}/${id}`, {
-//     method: "PATCH",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify(toApiPayload(data as ApplicationCreate)),
-//   });
-//   if (!res.ok) {
-//     throw new Error(`Failed to update application ${id}`);
-//   }
-
-//   const result: ApplicationRow = await res.json();
-//   return fromApiResponse(result);
-// }
-
-// export async function deleteApplication(id: string) {
-//   const res = await fetch(`${BASE_URL}/${id}`, { method: "DELETE" });
-//   if (!res.ok) {
-//     throw new Error(`Failed to delete application ${id}`);
-//   }
-//   return res.json();
-// }
