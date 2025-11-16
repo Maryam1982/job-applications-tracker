ced7de1ac41d3a022fbe45055dda69f485a55216
Maryam <mm.saleh.88@gmail.com>
2025-11-17 00:33:18 +0330

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import type { Metadata } from "next";
 import { Poppins, Inter } from "next/font/google";
 import "./globals.css";
@@ -28,6 +31,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${poppins.variable} ${inter.variable} antialiased`}>
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
